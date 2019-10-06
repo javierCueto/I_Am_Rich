f2a42260c825186570dc4cbc7c8436fc8a2bc8f1
Javier <javelie@outlook.com>
2019-10-06 16:05:33 -0600

    Datos

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by José Javier Cueto Mejía on 9/7/19.
+//  Copyright © 2019 José Javier Cueto Mejía. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
